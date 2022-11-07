dd05ecd4e2bb7159564c2cbe038f751f9b3c5274
chanbin213 <pcb0213@gmail.com>
2022-11-07 14:38:13 +0900

    JS for문을 사용하여 탭 기능 만들기

---
+++ js-practice-main/tab.js
@@ -0,0 +1,8 @@
+for(let i =0; i<3; i++){
+    $('.tab-button').eq(i).on('click',function(){
+        $('.tab-button').removeClass('orange');
+        $('.tab-button').eq(i).addClass('orange');
+        $('.tab-content').removeClass('show');
+        $('.tab-content').eq(i).addClass('show');
+    });
+}
