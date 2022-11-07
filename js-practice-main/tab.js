baa501f1bca0fe45af492b3ffb478ac0f21a316e
chanbin213 <pcb0213@gmail.com>
2022-11-07 15:26:24 +0900

    JS 이벤트 버블링과 관련 함수 사용하기

--- js-practice-main/tab.js
+++ js-practice-main/tab.js
@@ -1,8 +1,19 @@
-for(let i =0; i<3; i++){
-    $('.tab-button').eq(i).on('click',function(){
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(i).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-    });
+// for(let i =0; i<3; i++){
+//     $('.tab-button').eq(i).on('click',function(){
+//         탭열기(i);
+//     });
+// }
+
+function 탭열기(i){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(i).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(i).addClass('show');
+
 }
+
+$('.list').click(function(e){
+
+    탭열기(e.target.dataset.id);
+
+});
